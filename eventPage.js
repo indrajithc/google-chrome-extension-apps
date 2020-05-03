c792f36306148a2d47a7c30717b2940c3e973297
Indrajith C <indranspeaks@gmail.com>
2020-05-03 17:10:59 +0530

    speak it app added

--- eventPage.js
+++ eventPage.js
@@ -1,28 +1,17 @@
 const menuItem = {
-  id: "searchWiki",
-  title: "Search in Wikipedia",
+  id: "speakIt",
+  title: "Speak the text",
   contexts: ["selection"],
 };
 
 chrome.contextMenus.create(menuItem);
 
-function fixCodeUri(string) {
-  return encodeURI(string).replace(/%5B/g, "[").replace(/%5D/g, "]");
-}
-
 chrome.contextMenus.onClicked.addListener(function (clickData) {
-  if (clickData.menuItemId === "searchWiki" && clickData.selectionText) {
-    var wikiUrl = `https://en.wikipedia.org/wiki/${fixCodeUri(
-      clickData.selectionText,
-    )}`;
-    const createData = {
-      url: wikiUrl,
-      type: "popup",
-      top: 5,
-      left: 5,
-      width: screen.availWidth / 2,
-      height: screen.availHeight / 2,
-    };
-    chrome.windows.create(createData, function () {});
+  if (clickData.menuItemId === "speakIt" && clickData.selectionText) {
+    chrome.tts.speak(clickData.selectionText, { rate: 2.0 }, function () {
+      if (chrome.runtime.lastError) {
+        console.log("Error: " + chrome.runtime.lastError.message);
+      }
+    });
   }
 });
